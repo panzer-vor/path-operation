a7a5b454edd54ce39094e9714418e5fa4482e686
yorke <xyk0279@gmail.com>
2019-12-04 18:50:15 +0800

    docs: git ignore change

--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Config } from './types';
-declare const createInstance: <State>(state: State, config?: Config | undefined) => (path: string[], mapFn?: any) => any;
-export default createInstance;
